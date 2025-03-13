e533404a399347bd953964c0065c26f10801c02a
eden333333 <eden.neptune@gmail.com>
2025-03-13 15:00:49 +0200

    1332025

--- src/middleware/swagger-configuration.ts
+++ src/middleware/swagger-configuration.ts
@@ -10,10 +10,34 @@ const options = {
             description: "A complex express library for vacations posts"
         },
         servers: [
-            { url: "http://localhost:5000" }
-        ]
+            { url: "http://localhost:5500" }
+        ],
+        components: {
+            schemas: {
+                Login: {
+                    type: "object",
+                    required: ["email", "password"],
+                    properties:{
+                        email: { type: "string", example: "john@example.com" },
+                        password: { type: "string", example: "12345" },
+                    }
+                },
+                User: {
+                    type: "object",
+                    required: ["firstName", "lastName", "email", "password"],
+                    properties: {
+                        id: { type: "string", example: "64f5b3c7e4b08c1234567890" },
+                        firstName: { type: "string", example: "John" },
+                        lastName: { type: "string", example: "Doe" },
+                        email: { type: "string", example: "john@example.com" },
+                        password: { type: "string", example: "12345" },
+                        image: { type: "string", example: "pic.jpg" },
+                    },
+                },
+            },
+        }
     },
-    apis: ["./routes/*.js"]
+    apis: ["./src/routes/*.ts"]
 };
 
 const swaggerSpec = swaggerJSDoc(options);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,27 +4,56 @@ const router = express.Router();
 
 /**
  * @swagger
+ * tags:
+ *  - name: Authentication
+ *    description: user login and register
+ */
+/**
+ * @swagger
  * /api/auth/login:
  *   post:
- *    summary: Login to the application
+ *     summary: Login to the application
+ *     description: Authenticate a user and return a token.
  *     requestBody:
-*          email and password
- *     responses:token and user
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              $ref: "#/components/schemas/Login"
+ *     responses:
+ *       200:
+ *         description: Successfully logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *                 $ref: "#/components/schemas/User"
+ *       400:
+ *         description: Invalid credentials
  */
 router.post('/login', userController.login);
 
 /**
  * @swagger
  * /api/auth/register:
- *   post:  
- *    summary: Register to the application
- *    requestBody:
- *         firstName, lastName, birthDate, email, password
- *   responses: 201 ok-created
+ *   post:
+ *     summary: register to the application
+ *     description: Authenticate a user and return a token.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              $ref: "#/components/schemas/User"
+ *     responses:
+ *       200:
+ *         description: Successfully logged in
+ *         content:
+ *           application/json:
+ *           schema:
+ *              $ref: "#/components/schemas/User"
+ *       400:
+ *         description: Invalid credentials
  */
 router.post('/register', userController.addUser);
-router.get('/login', (req, res) => {
-    res.json({'field':1324})
-})
 
 export default router;
